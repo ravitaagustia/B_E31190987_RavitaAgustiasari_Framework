19bfeb28d4dc6686b0c7b7daaed761165bde452f
ravitaagustia <80375049+ravitaagustia@users.noreply.github.com>
2021-03-15 15:34:57 +0700

    Add files via upload

---
+++ Minggu 1_tugas1.php
@@ -0,0 +1,14 @@
+<?php
+//deklarasi class
+class buku {
+
+    // deklarasi property
+	    public $judul_buku;
+	    public $pengarang;
+	    public $penerbit;
+	    public $tahun_terbit;
+	    public $cetakan; 
+	    
+      }
+
+  ?>
