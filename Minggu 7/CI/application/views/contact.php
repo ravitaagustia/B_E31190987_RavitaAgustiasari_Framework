437788ab8452ed9606e92eabe587816d2582ffbf
ravitaagustia <80375049+ravitaagustia@users.noreply.github.com>
2021-03-22 21:53:43 +0700

    Minggu 7

---
+++ Minggu 7/CI/application/views/contact.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title> </title>
+</head>
+<body>
+<h2> Halaman Contact </h2>
+<p> selamat datang ini halaman contact </p>
+</body>
+</html>
---
+++ Minggu 7/CI/application/views/profil.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title> </title>
+</head>
+<body>
+<h2> Halaman Profil </h2>
+<p> selamat datang ini halaman profil </p>
+</body>
+</html>
